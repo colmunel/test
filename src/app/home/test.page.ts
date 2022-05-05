9cc8a5df3296d087f36c0d5d08297289dd0c067d
Colix <colix@MacBook-Pro-som-tillhor-Colix.local>
2022-05-05 08:00:06 +0200

    Test component

---
+++ src/app/home/test.page.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  templateUrl: './test.page.html',
+})
+export class TestPage{
+
+  constructor() { }
+
+}
